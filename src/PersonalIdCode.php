488994fe3e52a195f53a74a5fb1a1f553e95f4ef
Rene Korss <rene.korss@gmail.com>
2018-11-12 11:51:13 +0200

    Add API methods

--- src/PersonalIdCode.php
+++ src/PersonalIdCode.php
@@ -48,4 +48,64 @@ class PersonalIdCode
   
         return $modulo === 0 ? self::GENDER_FEMALE : self::GENDER_MALE;
     }
+
+    /**
+     * Get person birthday as \Datetime object
+     *
+     * @return \Datetime Person birthday
+     */
+    public function getBirthDate() : \Datetime
+    {
+        // @TODO
+    }
+
+    /**
+     * Get person age
+     *
+     * @return int Person age
+     */
+    public function getAge() : int
+    {
+        // @TODO
+    }
+
+    /**
+     * Get birth year
+     *
+     * @return int Person birth year
+     */
+    public function getBirthYear() : int
+    {
+        // @TODO
+    }
+
+    /**
+     * Get birth month
+     *
+     * @return int Person birth month
+     */
+    public function getBirthMonth() : int
+    {
+        // @TODO
+    }
+
+    /**
+     * Get birth day
+     *
+     * @return int Person birth day
+     */
+    public function getBirthDay() : int
+    {
+        // @TODO
+    }
+
+    /**
+     * Validate person code
+     *
+     * @return boolean True if valid personal code, false otherwise
+     */
+    public function validate() : bool
+    {
+        // @TODO
+    }
 }
