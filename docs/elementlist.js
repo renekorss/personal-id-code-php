28dbd2d3af78943e9f4908309b86ce237fd5144d
Rene Korss <rene.korss@gmail.com>
2021-09-28 21:15:03 +0300

    Update docs

--- docs/elementlist.js
+++
@@ -1,2 +0,0 @@
-var ApiGen = ApiGen || {};
-ApiGen.elements = [{"file":"namespace-RKD.PersonalIdCode.html","label":"RKD\\PersonalIdCode"},{"file":"class-RKD.PersonalIdCode.PersonalIdCode.html","label":"RKD\\PersonalIdCode\\PersonalIdCode"}];
