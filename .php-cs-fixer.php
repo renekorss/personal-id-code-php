65c8787f30b58a40f996e3cdd505eac1eccd0206
Rene Korss <rene.korss@gmail.com>
2021-09-28 21:14:58 +0300

    Update dependencies

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,7 +9,7 @@ return (new PhpCsFixer\Config)
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
     ])
     ->setUsingCache(false)
